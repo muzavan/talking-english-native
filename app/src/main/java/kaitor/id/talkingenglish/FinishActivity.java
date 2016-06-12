490e5441243819d34456f3b3ddaa656646f08a36
Muhammad Reza Irvanda <muzavan@gmail.com>
2016-06-13 02:37:02 +0700

    Badge added on Finish Activity

--- app/src/main/java/kaitor/id/talkingenglish/FinishActivity.java
+++ app/src/main/java/kaitor/id/talkingenglish/FinishActivity.java
@@ -26,8 +26,49 @@ public class FinishActivity extends AppCompatActivity {
         ImageView imageBadge = (ImageView) findViewById(R.id.image_badge);
         Button buttonContinue = (Button) findViewById(R.id.button_continue);
 
-        tvScore.setText(""+score);
+        //Beginner
+        if(topic.equalsIgnoreCase("home")){
+            imageBadge.setImageResource(R.drawable.home_badge);
+        }
+        if(topic.equalsIgnoreCase("fruits")){
+            imageBadge.setImageResource(R.drawable.fruits_badge);
+        }
+        if(topic.equalsIgnoreCase("number")){
+            imageBadge.setImageResource(R.drawable.number_badge);
+        }
+        if(topic.equalsIgnoreCase("family")){
+            imageBadge.setImageResource(R.drawable.family_badge);
+        }
 
+        //Intermediate
+        if(topic.equalsIgnoreCase("school")){
+            imageBadge.setImageResource(R.drawable.school_badge);
+        }
+        if(topic.equalsIgnoreCase("transportation")){
+            imageBadge.setImageResource(R.drawable.transportation_badge);
+        }
+        if(topic.equalsIgnoreCase("library")){
+            imageBadge.setImageResource(R.drawable.library_badge);
+        }
+        if(topic.equalsIgnoreCase("hobbies")){
+            imageBadge.setImageResource(R.drawable.hobbies_badge);
+        }
+
+        //Advanced
+        if(topic.equalsIgnoreCase("supermarket")){
+            imageBadge.setImageResource(R.drawable.supermarket_badge);
+        }
+        if(topic.equalsIgnoreCase("factory")){
+            imageBadge.setImageResource(R.drawable.factory_badge);
+        }
+        if(topic.equalsIgnoreCase("traffic")){
+            imageBadge.setImageResource(R.drawable.traffic_badge);
+        }
+        if(topic.equalsIgnoreCase("hospital")){
+            imageBadge.setImageResource(R.drawable.hospital_badge);
+        }
+        
+        tvScore.setText(""+score);
         buttonContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
